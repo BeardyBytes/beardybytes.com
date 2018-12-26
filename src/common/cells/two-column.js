4d10a418968be03054e770690b5795d2684921f8
Attila Bagossy <bagossyattila@outlook.com>
2018-12-26 21:45:30 +0100

    Two column cells added

---
+++ src/common/cells/two-column.js
@@ -0,0 +1,28 @@
+const { html } = require('common-tags');
+
+
+
+module.exports = {
+    cell: ({ left, right, options = {} }) => {
+        const classes = ['cell', 'two-column-cell'];
+
+        if (options.sticky) {
+            classes.push('sticky');
+        }
+
+        if (options.oversize) {
+            classes.push('oversize');
+        }
+
+        return html`
+            <div class="${classes.join(' ')}">
+                <div class="left">
+                    ${left}
+                </div>
+                <div class="right">
+                    ${right}
+                </div>
+            </div>
+        `;
+    }
+};
--- src/series/refactoring/posts/hello-world.js
+++ src/series/refactoring/posts/hello-world.js
@@ -2,6 +2,7 @@ const { html } = require('common-tags');
 const md = require('../../../common/cells/markdown');
 const { codeBlockWithNumbering, codeBlockWithoutNumbering } = require('../../../common/cells/code');
 const diffBlock = require('../../../common/cells/diff');
+const twoColumn = require('../../../common/cells/two-column');
 
 const post = {
     title: 'Hello, World!',
@@ -15,36 +16,53 @@ const post = {
         const cells = [
             md.cell`# Hello, World!`,
             md.cell`A paragraph of **markdown**.`,
-diffBlock.cell('c')`
-diff --git a/main.cpp b/main.cpp
-index c180659..6a07485 100644
---- a/main.cpp
-+++ b/main.cpp
-@@ -4,13 +4,11 @@
- #include math.h
- #include vector
- 
--using namespace std;
--
- float winWidth = 800.0, winHeight = 700.0;
- GLint dragged = 1;
- vec2 startpoint, endpoint;
--vectorvec2 controllpoints = {};
--vectorvec2 points = {};
-+std::vectorec2 controllpoints = {};
-+std::vectorvec2 points = {};
- 
- 
- mat4 M = {
-@@ -78,7 +76,7 @@ void bspline() {
- };
- 
- 
--GLint getActivePoint1(vector<vec2> p, GLint size, GLint sens, GLint x, GLint y)
-+GLint getActivePoint1(std::vector<vec2> p, GLint size, GLint sens, GLint x, GLint y)
- {
-     GLint i, s = sens * sens;
-`
+            twoColumn.cell({
+                options: {
+                    sticky: true,
+                    oversize: true
+                },
+                left: md`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum lectus orci, sed iaculis est pulvinar tempus. Suspendisse potenti. Praesent gravida iaculis eleifend. 
+
+Cras faucibus nisi a nulla vulputate, eget iaculis est vulputate. In a justo sem. Vivamus tempor hendrerit ante sed dignissim. Integer vel faucibus orci. 
+
+Pellentesque tempus justo ex. Donec eu nibh sed nibh vestibulum egestas quis nec elit. Cras aliquam erat id orci porttitor congue.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum lectus orci, sed iaculis est pulvinar tempus. Suspendisse potenti. Praesent gravida iaculis eleifend. 
+
+Cras faucibus nisi a nulla vulputate, eget iaculis est vulputate. In a justo sem. Vivamus tempor hendrerit ante sed dignissim. Integer vel faucibus orci. 
+
+Pellentesque tempus justo ex. Donec eu nibh sed nibh vestibulum egestas quis nec elit. Cras aliquam erat id orci porttitor congue.`,
+                right: codeBlockWithNumbering('javascript')`
+                function main() {
+                    console.log('hello, world');
+                ${"Here, we are printing hello!"}
+                }`
+            }),
+            twoColumn.cell({
+                options: {
+                    sticky: true
+                },
+                right: md`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum lectus orci, sed iaculis est pulvinar tempus. Suspendisse potenti. Praesent gravida iaculis eleifend. 
+
+Cras faucibus nisi a nulla vulputate, eget iaculis est vulputate. In a justo sem. Vivamus tempor hendrerit ante sed dignissim. Integer vel faucibus orci. 
+
+Pellentesque tempus justo ex. Donec eu nibh sed nibh vestibulum egestas quis nec elit. Cras aliquam erat id orci porttitor congue.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum lectus orci, sed iaculis est pulvinar tempus. Suspendisse potenti. Praesent gravida iaculis eleifend. 
+
+Cras faucibus nisi a nulla vulputate, eget iaculis est vulputate. In a justo sem. Vivamus tempor hendrerit ante sed dignissim. Integer vel faucibus orci. 
+
+Pellentesque tempus justo ex. Donec eu nibh sed nibh vestibulum egestas quis nec elit. Cras aliquam erat id orci porttitor congue.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum lectus orci, sed iaculis est pulvinar tempus. Suspendisse potenti. Praesent gravida iaculis eleifend. 
+
+Cras faucibus nisi a nulla vulputate, eget iaculis est vulputate. In a justo sem. Vivamus tempor hendrerit ante sed dignissim. Integer vel faucibus orci. 
+
+Pellentesque tempus justo ex. Donec eu nibh sed nibh vestibulum egestas quis nec elit. Cras aliquam erat id orci porttitor congue.`,
+                left: md`asd`
+            })
         ];
 
         return cells.join('\n');

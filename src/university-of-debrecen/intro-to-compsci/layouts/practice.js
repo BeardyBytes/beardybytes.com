0f45db1247e299ae1b61f354ee0259c4c297c637
Attila Bagossy <bagossyattila@outlook.com>
2020-09-12 14:15:35 +0200

    feat: katex css added to the practice layout

--- src/university-of-debrecen/intro-to-compsci/layouts/practice.js
+++ src/university-of-debrecen/intro-to-compsci/layouts/practice.js
@@ -37,6 +37,8 @@ const elements = {
 
       <link rel="stylesheet" href="/resources/css/normalize.css" />
       <link rel="stylesheet" href="/resources/css/main.css" />
+
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
     `
   },
   header() {

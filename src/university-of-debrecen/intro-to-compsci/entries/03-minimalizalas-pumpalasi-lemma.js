81e61ddda9497de8a143276c22c473da327e0f1c
Attila Bagossy <bagossyattila@outlook.com>
2020-09-27 11:34:47 +0200

    feat: dfa minimization table and its styling for the third week's compsci class

--- src/university-of-debrecen/intro-to-compsci/entries/03-minimalizalas-pumpalasi-lemma.js
+++ src/university-of-debrecen/intro-to-compsci/entries/03-minimalizalas-pumpalasi-lemma.js
@@ -2,6 +2,62 @@ const { DateTime } = require('luxon')
 const kroki = require('../../../common/cells/kroki')
 const md = require('../../../common/cells/markdown')
 const { section, subsection, subsubsection } = require('../../../common/cells/heading')
+const tex = require('../../../common/tex')
+
+function dfaMinimizationTable(alphabet, groups) {
+  function statesHeadOfGroup(group) {
+    const isFirstState = i => i == 0
+
+    return group.states.map((state, index) => `
+      <th class="${isFirstState(index) ? "first-state-of-group" : ""}">${state.name}</th>
+    `).join('')
+  }
+
+  const head = `
+<thead>
+  <tr class="groups-head-row">
+    <th>${tex`\\Sigma`}</th>
+    ${groups.map(g => `<th colspan="${g.states.length}">${g.name}</th>`).join('')}
+  </tr>
+  <tr class="states-head-row">
+    <th>&nbsp;</th>
+    ${groups
+      .map(statesHeadOfGroup)
+      .join('')
+    }
+  </tr>
+</thead>
+`
+
+  function transitionRowSegment(symbol, group) {
+    const isFirstState = i => i == 0
+
+    return group.states.map((state, index) => `
+      <td class="${isFirstState(index) ? "first-state-of-group" : ""}">${state.transitions[symbol] || ''}</td>
+    `).join('')
+  }
+
+  const body = `
+<tbody>
+    ${alphabet.map(symbol => `
+      <tr>
+        <td>${tex`${symbol}`}</td>
+        ${groups
+          .map(g => transitionRowSegment(symbol, g))
+          .join('')
+        }
+      </tr>
+    `).join('')}
+</tbody>
+`
+
+return `
+<table class="dfa-minimization-table">
+  ${head}
+  ${body}
+</table>
+`
+}
 
 const cells = [
   md.cell`
@@ -14,35 +70,50 @@ A gyakorlathoz tartozó feladatsor elérhető a következő linken:
   section.cell`DFA minimalizálás`,
   subsection.cell`A DFA minimalizálás algoritmusa`,
   md.cell`
-Ez a feladat még nem automatákkal, hanem nyelvekkel kapcsolatos. Meg kell mondanunk, hogy egy adott egyenlőség igaz-e, ha pedig nem, akkor mutatnunk kell egy ellenpéldát.
+A minimalizálás algoritmusa részletesen, bizonyítással együtt elérhető a következő forrásban:
 
-> a. Nem teljesül. Ellenpélda: $L_{1} = \\{a\\}$, $L_{2} = \\{b\\}$.
+  > [Bach Iván: Formális nyelvek (2. kiadás); 43-49. oldal](https://www.typotex.hu/download/formalisnyelvek.pdf)
 
-> b. Teljesül.
+A következőkben röviden ismertetném az algoritmust, ahogy a gyakorlaton is felírtuk.
 
-> c. Teljesül.
+  0. Legyen adott az $M = (Q, \\Sigma, q_{0}, A, \\delta)$ teljesen specifikált determinisztikus véges automata.
+  1. Bontsuk az automata állapotait két csoportra: elutasító állapotok ($Q$ azon elemei, melyek nincsenek benne $A$-ban) és elfogadó állapotok ($A$ elemei).
+  2. Addig végezzünk csoportbontást a lenti lépéseknek megfelelően, ameddig lehetséges:
+     1. Minden állapotra írjuk fel, hogy mely $\\Sigma$-beli betűre mely csoportbeli állapotba lépünk át belőle. Azaz, nem a tényleges állapot az érdekes, hanem a csoport, melyben a célállapot található.
+     2. Vizsgáljuk meg a csoportokat! Ha egy csoport minden állapota azonosan viselkedik, azaz azonos bemenetre azonos csoportba visz át, akkor azzal a csoporttal nem csinálunk semmit. Ellenkező esetben azonban felbontjuk a csoportot annyi csoportra, ahány különböző viselkedést találunk benne. A különböző viselkedés azt jelenti, hogy ugyanazon bementre más csoportba visznek át az egyes állapotok. Az egyes csoportokba azok az állapotok fognak kerülni, melyek azonosan viselkednek.
+  3. Ha már nem tudunk további csoportbontást végezni (azaz minden csoport azonosan viselkedő állapotokat tartalmaz), akkor készen vagyunk. Az $M^{\\prime} = (Q^{\\prime}, \\Sigma^{\\prime}, q_{0}^{\\prime}, A^{\\prime}, \\delta^{\\prime})$ minimális automatát a következőképpen írhatjuk fel:
+     * $Q^{\\prime}$: Minden csoport egy állapotnak felel meg.
+     * $\\Sigma^{\\prime}$: A bemeneti ábécé nem változik, megegyezik $\\Sigma$-val.
+     * $q_{0}^{\\prime}$: A kezdőállapot az a csoport lesz, melyben az eredeti kezdőállapot található.
+     * $A^{\\prime}$: Minden olyan csoport elfogadó állapot lesz, mely az eredeti automata elfogadó állapotait tartalmazza.
+     * $\\delta^{\\prime}$: Az állapotátmenet függvényt az egyes csoportokban található állapotok viselkedése alapján írhatjuk fel.
 
-> d. Nem teljesül. Ellenpélda: $L_{1} = \\{a\\}$, $L_{2} = \\{aa\\}$.
-
-> e. Nem teljesül. Ellenpélda: $L_{1} = \\{a\\}$, $L_{2} = \\{b\\}$.
-
-> f. Teljesül.
+A fenti algoritmust egy táblázat segítségével szoktuk végrehajtani. A \`2.55. a)\` részfeladatban megjelenik e táblázat kitöltésének pontos módja. 
 `,
-  section.cell`Determinisztikus véges automaták`,
-  subsection.cell`2.2 feladat`,
+  subsection.cell`2.55. feladat`,
+  subsubsection.cell`2.55. a.`,
   md.cell`
-  Egy jó megközelítés, ha felíunk néhány szót, majd végigkövetjük az automata viselkedését ezekre a szavakra. Ezt követően, a szavak hasonlóságai vagy eltérései alapján már könnyebben megállapíthatjuk, hogy az automata "mit is csinál" pontosan.
+A minimalizálási algoritmus első lépésének megfelelően, azzal kezdjük a megoldást, hogy az automata állapotait két csoportra bontjuk. Az I. csoportba kerülnek az elutasító állapotok, míg a II. csoportba az elfogadó állapotok.
 
-> a. Azon szavakból álló nyelv, melyek tartalmazzák az $aaba$ részszót.
-
-> b. Azon szavakból álló nyelv, melyek az $aaba$ részszóra végződnek.
-
-> c. Azon szavakból álló nyelv, melyek az $aaba$ részszóval kezdődnek.
-
-> d. Az üres szó ($\\lambda$), valamint azon szavak, melyek $a$-val kezdődnek és $b$-re végződnek.
-
-> e. Az üres szó ($\\lambda$), valamint azon szavak, melyek $ab$ és $ba$ blokkokból állnak. Azaz $\\{ab, ba\\}^{*}$.
+A csoportosítást, majd csoportbontást egy táblázat segítségével végezzük, melyből minden egyes csoportbontástkor újat készítünk. A táblázat oszlopait az eredeti automata állapotai, sorait pedig a bemeneti ábécé betűi alkotják. Az egyes cellákba az adott állapotátmenetnek megfelelő csoportot írjuk.
 `,
+  dfaMinimizationTable(['a', 'b'], [
+    {
+      name: 'I.',
+      states: [
+        { name: '1', transitions: {} },
+        { name: '2', transitions: {} },
+        { name: '3', transitions: {} },
+        { name: '4', transitions: {} },
+      ]
+    },
+    {
+      name: 'II.',
+      states: [
+        { name: '5', transitions: {} },
+      ]
+    }
+  ]),
   subsection.cell`2.1 feladat`,
   subsubsection.cell`2.1 a.`,
   kroki.cell('graphviz', 'svg')`
@@ -75,7 +146,7 @@ const meta = {
 const content = {
   title: '3. gyakorlat – Minimalizálás, pumpálási lemma',
   excerpt: 'Determinisztikus véges automatákat minimalizáltunk, valamint a pumpálási lemmával bizonyítottuk nyelvekről, hogy nem regulárisak.',
-  cells,
+  cells
 }
 
 module.exports = {

3bb738e24d56ffc5dc19772d672523ab46912ceb
Attila Bagossy <bagossyattila@outlook.com>
2020-11-10 20:01:50 +0100

    feat: post added for week 9 (without the exercise PDF for now)

---
+++ src/university-of-debrecen/intro-to-compsci/entries/09-pumpalasi-lemma-kornyezetfuggetlen-nyelvekre-veremautomatak.js
@@ -0,0 +1,30 @@
+const { DateTime } = require('luxon')
+const md = require('../../../common/cells/markdown')
+
+const cells = [
+  md.cell`
+A gyakorlathoz tartozó feladatsor elérhető a következő linken:
+
+> [09-pumpalasi-lemma-kornyezetfuggetlen-nyelvekre-veremautomatak-feladatsor.pdf](./files/09-pumpalasi-lemma-kornyezetfuggetlen-nyelvekre-veremautomatak-feladatsor.pdf)
+`,
+]
+
+const meta = {
+  order: 9,
+  layout: 'practice',
+  urlTitle: '09-pumpalasi-lemma-kornyezetfuggetlen-nyelvekre-veremautomatak',
+  publishedAt: DateTime.local(2020, 11, 10, 20, 00),
+  draft: false,
+}
+
+const content = {
+  title: '9. gyakorlat – Pumpálási lemma környezetfüggetlen nyelvekre, veremautomaták',
+  excerpt:
+    'A környezetfüggetlen nyelvekre vonatkozó pumpálási lemmát alkalmazva megmutatjuk nyelvekről, hogy nem lehet őket környezetfüggetlen grammatikával generálni. A gyakorlat második felében pedig egy új típusú automatával, a veremautomatával foglalkozunk.',
+  cells,
+}
+
+module.exports = {
+  meta,
+  content,
+}

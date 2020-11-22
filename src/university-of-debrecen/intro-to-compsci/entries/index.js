1a84a60d63e54d5a918098fd96345261377f790e
Attila Bagossy <bagossyattila@outlook.com>
2020-11-22 01:25:01 +0100

    fix: removed the accidental filtering of compsci entries

--- src/university-of-debrecen/intro-to-compsci/entries/index.js
+++ src/university-of-debrecen/intro-to-compsci/entries/index.js
@@ -1,7 +1,7 @@
 const requireAll = require('../../../util/require-all')
 
 module.exports = (function entriesFactory() {
-  const entries = requireAll(__dirname, ['index.js']).filter((a) => a.meta.order >= 7)
+  const entries = requireAll(__dirname, ['index.js'])
 
   entries.sort((a, b) => a.meta.order - b.meta.order)
 
